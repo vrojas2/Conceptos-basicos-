457c285e0f570baf39b12704f532a4753b2198ef
Jonathanrrs12 <yoni_12rrs@hotmail.com>
2019-02-05 19:36:22 -0600

    avance

---
+++ conceptos-basicos/auto.js
@@ -0,0 +1,10 @@
+class auto {
+    constructor(marca, modelo, color, kilometraje) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.kilometraje = kilometraje;
+        this.estado = "Apagado";
+        
+    }
+}
